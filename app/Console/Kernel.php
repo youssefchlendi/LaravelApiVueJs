f4602e41e9ca80f250bd9793e89013b5c3cda7c6
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-01-28 23:28:42 +0100

    update all

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function(){
+            info('khdemt');
+        })->everyMinute();
     }
 
     /**
